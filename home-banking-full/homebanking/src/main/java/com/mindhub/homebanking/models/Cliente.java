c4cce516bc1299502782d73df8aa2584c79f78b6
BetoCochas <oscargio86@hotmail.com>
2023-01-23 23:39:00 -0500

    commit1

---
+++ home-banking-full/homebanking/src/main/java/com/mindhub/homebanking/models/Cliente.java
@@ -0,0 +1,66 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+   @Id
+   @GeneratedValue
+   @GenericGenerator(name="native",strategy="native")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
