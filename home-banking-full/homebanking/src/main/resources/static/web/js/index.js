85717bd3e6d9e07f82bd4edcf893c8a68ab17146
BetoCochas <oscargio86@hotmail.com>
2023-02-11 23:06:27 -0500

    'web'

---
+++ home-banking-full/homebanking/src/main/resources/static/web/js/index.js
@@ -0,0 +1,9 @@
+function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var clock = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = clock;
+  }
+  setInterval(displayTime, 1000);
