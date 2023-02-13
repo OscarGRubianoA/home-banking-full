794db56c2b05aed69294d3c9bcf85da6dd57e536
BetoCochas <oscargio86@hotmail.com>
2023-02-12 22:39:07 -0500

    account,js

--- home-banking-full/homebanking/src/main/resources/static/web/js/account.js
+++ home-banking-full/homebanking/src/main/resources/static/web/js/account.js
@@ -1,24 +1,23 @@
-const{createApp}=Vue;
+const { createApp } = Vue;
 
 createApp({
-data(){
-return{
-data:[],
-state:"accounts",
-limit:5,
-}
-}created(){
-this.loadData();
-},methods:{
-loadData:function(){
-axios.get("http://localhost:8080/api/clients/1")
-.then(response=>
-{
-this.data=response.data;
-this.data.accounts.sort((a,b)=>a.id-b.id);
-console.log(this.data);
-})
-.catch(err=>console.log(err));
-},
-}
-}),mount('app');
+    data() {
+        return {
+            data: [],
+            state: "accounts",
+            limit: 5,
+        }
+    }, created() {
+        this.loadData();
+    }, methods: {
+        loadData: function () {
+            axios.get("http://localhost:8080/api/clients/1")
+                .then(response => {
+                    this.data = response.data;
+                    this.data.accounts.sort((a, b) => a.id - b.id);
+                    console.log(this.data);
+                })
+                .catch(err => console.log(err));
+        },
+    }
+}), mount('app');
